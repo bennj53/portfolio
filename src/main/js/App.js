40b79a1531114902681942ab9ac5dd5cc47ff5bf
bennj53 <benoitjulliot@gmail.com>
2019-07-29 00:42:22 +0200

    add SpringBoot and React

---
+++ src/main/js/App.js
@@ -0,0 +1,15 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+class App extends React.Component {
+
+  render() {
+     return (
+     
+        <div>
+           <p>liste des étudiants</p>
+        </div>
+     
+     )
+  }
+}
